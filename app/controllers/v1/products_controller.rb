e6db191091a41e0510cea241771999a684a38860
deniskem <denis.kem@gmail.com>
2018-05-01 18:20:27 +0700

    fix products controller

--- app/controllers/v1/products_controller.rb
+++ app/controllers/v1/products_controller.rb
@@ -2,6 +2,6 @@ class V1::ProductsController < ResourcesController
   private
   
   def permitted_attributes
-    [:category_id, :name, :price]
+    super + [:category_id, :name, :price]
   end
 end
