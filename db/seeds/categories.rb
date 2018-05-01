3bfe505d3bca0a268287af3a4ff9791ae0475a09
deniskem <denis.kem@gmail.com>
2018-05-02 02:33:09 +0700

    refactor

--- db/seeds/categories.rb
+++ db/seeds/categories.rb
@@ -10,11 +10,11 @@
   {
     name: 'Cars', products: [{name: 'Lada Granta', price: 400_000}, {name: 'Renault Logan', price: 540_000}]
   }
-].each do |category_attrs|
-  category = Category.find_or_create_by!(name: category_attrs[:name])
-  category_attrs[:products].each do |product_attrs|
-    Product.find_or_create_by!(category: category, name: product_attrs[:name]) do |product|
-      product.price = product_attrs[:price]
+].each do |name:, products:|
+  category = Category.find_or_create_by!(name: name)
+  products.each do |name:, price:|
+    Product.find_or_create_by!(category: category, name: name) do |product|
+      product.price = price
     end
   end
 end
